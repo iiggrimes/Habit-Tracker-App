128c7a4e54cc584dbc694c9acf42e155cc222541
iiggrimes <iiggrimes@gmail.com>
2023-03-23 01:54:11 -0400

    Empty spring

---
+++ src/test/java/com/dev/habittracker/HabitTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.habittracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
