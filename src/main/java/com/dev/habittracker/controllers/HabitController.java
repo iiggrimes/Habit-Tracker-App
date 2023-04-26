186f78fad2a2cd742c22a1f5ae444c08facabf46
iiggrimes <iiggrimes@gmail.com>
2023-04-26 11:40:31 -0400

    adding to controllers

--- src/main/java/com/dev/habittracker/controllers/HabitController.java
+++ src/main/java/com/dev/habittracker/controllers/HabitController.java
@@ -1,4 +1,48 @@
 package com.dev.habittracker.controllers;
 
+import com.dev.habittracker.models.Habit;
+import com.dev.habittracker.repositories.HabitRepository;
+import com.dev.habittracker.services.HabitService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/habits")
 public class HabitController {
+    @Autowired
+    private HabitRepository habitRepository;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Habit> getHabitById(@PathVariable Long id){
+        Optional<Habit> habitOptional = habitRepository.findById(id);
+        return habitOptional.map(habit -> ResponseEntity.ok().body(habit)).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<Habit> createHabit(@RequestBody Habit habit) {
+        Habit savedHabit = habitRepository.save(habit);
+        return ResponseEntity.created(URI.create("/habits" + savedHabit.getId())).body(savedHabit);
+
+
+    }
+
+    @GetMapping("/{id}/records")
+    public ResponseEntity<List<Record>> getHabitRecords(@PathVariable Long id) {
+        Optional<Habit> habitOptional = habitRepository.findById(id);
+        if (habitOptional.isPresent()) {
+            Habit habit = habitOptional.get();
+            return ResponseEntity.ok().body(habit.getRecords());
+        } else {
+            return ResponseEntity.notFound().build();
+
+        }
+    }
+
+
+
 }
--- src/main/java/com/dev/habittracker/controllers/UserController.java
+++ src/main/java/com/dev/habittracker/controllers/UserController.java
@@ -1,4 +1,47 @@
 package com.dev.habittracker.controllers;
 
+import com.dev.habittracker.models.User;
+import com.dev.habittracker.repositories.UserRepository;
+import com.dev.habittracker.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/{username}")
+    public ResponseEntity<User> getUserByUsername(@PathVariable String username) {
+        Optional<User> userOptional = Optional.ofNullable(userRepository.findByUsername(username)); //hard to warp
+        return userOptional.map(user -> ResponseEntity.ok().body(user)).orElseGet(() -> ResponseEntity.notFound().build());
+
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User savedUser = userRepository.save(user);
+        return ResponseEntity.created(URI.create("/users/" + savedUser.getUsername())).body(savedUser);
+
+    }
+
+
+
+
+
+/*    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@RequestBody User user){
+        userService.saveUser(user);
+        return ResponseEntity.ok(new MessageResponse("User registered successfully"));
+
+    }*/
 }
